7dfc8ff6664b4d0f43fd77a2ef0605a6e7be335b
tomgrandson <tomas.grandson@gmail.com>
2019-10-29 22:01:37 +0100

    splash i login view

---
+++ ECOCITY_CLIENT_MOBILE/ios/HubInfo.h
@@ -0,0 +1,13 @@
+//
+//  HubInfo.h
+//  ECOCITY_RN
+//
+//  Created by Tom Grandson on 24/10/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#ifndef HubInfo_h
+#define HubInfo_h
+
+
+#endif /* HubInfo_h */
